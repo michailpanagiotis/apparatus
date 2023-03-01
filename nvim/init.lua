c5d71ef55967fc82d5a1a72ad7b0f8540635e7db
Panos Michail <michailpanagiotis@gmail.com>
2023-03-01 08:45:58 +0000

    WIP

--- nvim/init.lua
+++ nvim/init.lua
@@ -202,8 +202,6 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Search/replace
-vim.keymap.set('v', '<C-R>', [["hy:%s/<C-r>h//gc<left><left><left>]], {});
 
 require 'user/vim'
 require 'user/core'
--- nvim/lua/user/vim.lua
+++ nvim/lua/user/vim.lua
@@ -2,4 +2,7 @@ vim.cmd([[
   " matze/vim-move
   let g:move_key_modifier = 'S'
   let g:move_key_modifier_visualmode = 'S'
+
+  " Search/replace
+  vnoremap <C-R> "hy:%s/<C-r>h//gc<left><left><left>
 ]])
