09acfc42c0d683acbc29d00eda7008635af79cf2
Panos Michail <michailpanagiotis@gmail.com>
2023-03-13 09:11:00 +0000

    WIP

--- nvim/lua/user/theme.lua
+++ nvim/lua/user/theme.lua
@@ -4,6 +4,7 @@ function M.setup(use)
   use 'RRethy/nvim-base16'
   use 'sainnhe/everforest'
   use 'sainnhe/gruvbox-material'
+  use { "briones-gabriel/darcula-solid.nvim", requires = "rktjmp/lush.nvim" }
 
   -- Set colorscheme
   vim.o.termguicolors = true
@@ -15,7 +16,7 @@ function M.setup(use)
   vim.g.monokaipro_flat_float = false
   vim.g.monokaipro_sidebars = { "packer", "Trouble" }
   vim.g.sonokai_style = 'default'
-  vim.cmd [[colorscheme base16-everforest]]
+  vim.cmd [[colorscheme everforest]]
 end
 
 return M
