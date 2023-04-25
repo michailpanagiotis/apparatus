b2463b833fe59af37cdff2814ed9927a3fe4a82b
Panos Michail <michailpanagiotis@gmail.com>
2023-04-25 08:05:29 +0000

    Python linting

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -8,12 +8,15 @@ Plugin.config = function ()
   null_ls.setup {
     sources = {
       null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.diagnostics.flake8.with({
+        args = { "--format", "default", "--config", "/root/.apparatus/.flake8", "--stdin-display-name", "$FILENAME", "-" }
+      }),
       null_ls.builtins.diagnostics.pylint.with({
         diagnostics_postprocess = function(diagnostic)
           diagnostic.code = diagnostic.message_id
         end,
+        args = { "--rcfile", "/root/.apparatus/.pylintrc", "--from-stdin", "$FILENAME", "-f", "json" }
       }),
-      null_ls.builtins.formatting.isort,
       null_ls.builtins.formatting.black,
     },
     update_in_insert = false,
