d762ee0cd026767cd8907c7c430309cdc88e81f4
Panos Michail <michailpanagiotis@gmail.com>
2023-04-28 10:19:40 +0000

    WIP

--- install_neovim.sh
+++ install_neovim.sh
@@ -2,6 +2,6 @@ apt-get install -y curl g++
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 ./nvim.appimage --appimage-extract
 ./squashfs-root/AppRun --version
-mv squashfs-root /
-ln -s /squashfs-root/AppRun /usr/bin/nvim
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 nvim
