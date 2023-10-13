32ac16c3b469ab3957abb720309cae9ee3063277
Panos Michail <michailpanagiotis@gmail.com>
2023-10-13 10:48:05 +0000

    ALE black

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -6,10 +6,14 @@ Plugin.event = {'BufReadPre', 'BufNewFile'}
 vim.cmd [[
 let g:ale_use_neovim_diagnostics_api = 1
 let g:ale_virtualtext_cursor=0
+let g:ale_fix_on_save = 1
 let g:ale_python_ruff_options = '--config $HOME/.apparatus/.ruff.toml'
 let g:ale_linters = {
 \   'javascript': ['eslint'],
 \   'python': ['ruff'],
+\}
+let g:ale_fixers = {
+\   'python': ['black'],
 \}
 ]]
 return Plugin
