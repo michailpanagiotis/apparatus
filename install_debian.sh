bd554d1e33b275a96b027f99e4ca27d0c6012723
Panos Michail <michailpanagiotis@gmail.com>
2024-08-02 11:23:59 +0300

    WIP

--- install_debian.sh
+++ install_debian.sh
@@ -4,7 +4,7 @@ set -e
 install_base() {
   echo Installing packages...
   apt-get update
-  apt-get install wget git curl zsh jq wireguard fzf build-essential kitty-terminfo
+  apt-get install wget git curl zsh jq wireguard fzf build-essential kitty-terminfo ncdu htop
   sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
 
@@ -15,62 +15,12 @@ install_toolkit() {
   source /tmp/bundle.bash
 }
 
-install_git() {
-  wget -O $HOME/.gitconfig -q https://raw.githubusercontent.com/michailpanagiotis/apparatus/master/git/gitconfig
-  if ! test -f ~/.ssh/id_ed25519.pub; then
-    ssh-keygen -t ed25519 -C "michailpanagiotis@gmail.com"
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_ed25519
-    echo Add the following public key to github:
-    cat ~/.ssh/id_ed25519.pub
-  fi
-  ssh -T git@github.com
-}
-
-install_apparatus() {
-  if [ ! -d "$HOME/.apparatus" ]
-  then
-    git clone https://github.com/michailpanagiotis/apparatus $HOME/.apparatus
-  else
-    echo "Apparatus exists"
-  fi
-
-  if [ ! -f "$HOME/.zshrc" ]
-  then
-    ln -s $HOME/.apparatus/.zshrc $HOME/.zshrc
-  else
-    echo "Zsh config exists"
-  fi
-
-  if [ ! -d "$HOME/.config/nvim" ]
-  then
-    mkdir -p $HOME/.config
-    ln -s $HOME/.apparatus/nvim $HOME/.config/nvim
-  else
-    echo "Nvim config exists"
-  fi
-}
-
-install_nvim() {
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  chmod +x nvim.appimage
-  ./nvim.appimage --appimage-extract
-  mv squashfs-root /
-  ln -s /squashfs-root/AppRun /usr/bin/nvim
-  mkdir -p ~/.vim/files/info
-  rm nvim.appimage
-  update-alternatives --install /usr/bin/ex ex /usr/bin/nvim 110
-  update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 110
-  update-alternatives --install /usr/bin/view view /usr/bin/nvim 110
-  update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 110
-  update-alternatives --install /usr/bin/vimdiff vimdiff /usr/bin/nvim 110
-  nvim
-}
-
 install_base
 install_toolkit
 PEER_NUMBER=$(with_validate 'input "Please enter the peer number"' validate_present)
 source <(curl -s https://raw.githubusercontent.com/michailpanagiotis/apparatus/master/scripts/install/install_wireguard.sh)
 source <(curl -s https://raw.githubusercontent.com/michailpanagiotis/apparatus/master/scripts/install/install_github.sh)
-install_apparatus
-install_nvim
+source <(curl -s https://raw.githubusercontent.com/michailpanagiotis/apparatus/master/scripts/install/install_apparatus.sh)
+source <(curl -s https://raw.githubusercontent.com/michailpanagiotis/apparatus/master/scripts/install/install_nvim.sh)
+
+sed 's/#SystemMaxUse=/SystemMaxUse=100M/g' -i /etc/systemd/journald.conf && systemctl restart systemd-journald.service
