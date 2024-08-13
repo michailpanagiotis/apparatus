84d31cd0582c5742d4fa0d03f0a77c0b7c1fcb53
Panos Michail <michailpanagiotis@gmail.com>
2024-08-13 06:50:44 +0000

    Split wip

--- scripts/ffmpeg/split.sh
+++ scripts/ffmpeg/split.sh
@@ -7,27 +7,45 @@ for n in $(seq 2 $#); do
   # echo INDEX $n
   date=${!n}
   # echo DATE $date
-  curr=$(date -j -f "%H:%M:%S" "$date" +%s)
+
+  if [ "$(uname)" == "Darwin" ]; then
+      # Do something under Mac OS X platform
+      curr=$(date -j -f "%H:%M:%S" "$date" +%s)
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      # Do something under GNU/Linux platform
+      curr=$(date --date="$date" "+%s")
+  fi
+
   # echo $curr
   next=$(echo "$curr + 1" | bc)
   prev=$(echo "$curr - 1" | bc)
 
-  to=$(date -r $prev "+%H:%M:%S")
+  if [ "$(uname)" == "Darwin" ]; then
+      to=$(date -r $prev "+%H:%M:%S")
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      to=$(date -d "@$prev" "+%H:%M:%S")
+  fi
 
   output=$(echo "cut_${from}_${to}.mp4")
 
-  command=$(echo ffmpeg -i \""$1"\" -ss $from -to $to -c:v libx265 -c:a copy -preset ultrafast -x265-params crf=30 \"$output\" ";")
+  command=$(echo ffmpeg -i \""$1"\" -ss $from -to $to -c:v libx265 -c:a copy -preset ultrafast -x265-params crf=30 -movflags faststart -maxrate 2M -bufsize 2M \"$output\" ";")
 
   # echo $command
   commands+=( $command )
-  from=$(date -r $next "+%H:%M:%S")
+
+  if [ "$(uname)" == "Darwin" ]; then
+      from=$(date -r $next "+%H:%M:%S")
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      from=$(date -d "@$next" "+%H:%M:%S")
+  fi
 done
 
 output=$(echo "cut_${from}.mp4")
-command=$(echo ffmpeg -i \""$1"\" -ss $from -c:v libx265 -c:a copy -preset ultrafast -x265-params crf=30 \"$output\")
+command=$(echo ffmpeg -i \""$1"\" -ss $from -c:v libx265 -c:a copy -preset ultrafast -x265-params crf=30 -movflags faststart -maxrate 2M -bufsize 2M \"$output\")
 # echo $command
 commands+=( $command )
 
 full_command=$(echo ${commands[@]})
 
+echo $full_command
 eval $full_command
