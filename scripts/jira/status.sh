eb69c7ef5f349ab601323c42d4de78f580b90a28
Panos Michail <michailpanagiotis@gmail.com>
2025-02-18 12:59:13 +0000

    WIP

--- scripts/jira/status.sh
+++ scripts/jira/status.sh
@@ -8,7 +8,7 @@ curl -s --request POST ${JIRA_API_URL}/search -u "${JIRA_API_USER}:${JIRA_API_TO
 echo '  My features:'
 curl -s --request POST ${JIRA_API_URL}/search -u "${JIRA_API_USER}:${JIRA_API_TOKEN}" --json '{"jql":"status NOT IN (Done, Closed) AND project IN (PAYM) AND assignee = currentUser() AND status != \"Blocked / On Hold\" ORDER BY created DESC"}' | jq -r '.issues[].key' | sed 's/^/\thttps:\/\/talentdesk.atlassian.net\/browse\//g'
 
-if [ ! -z "${SHOW_BUG_BOARD}" ];
+if [ ! -z "${SHOW_BUGBOARD}" ];
 then
     echo '  Bug board:'
     curl -s --request POST ${JIRA_API_URL}/search -u "${JIRA_API_USER}:${JIRA_API_TOKEN}" --json '{"jql":"status NOT IN (Done, Closed) AND project IN (BT) AND assignee IN (currentUser(), empty) AND status != \"Blocked / On Hold\" AND \"Team[Dropdown]\" = \"Payments development\" ORDER BY created DESC"}' | jq -r '.issues[].key' | sed 's/^/\thttps:\/\/talentdesk.atlassian.net\/browse\//g'
