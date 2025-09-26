4526919c9ba977d51fb136f965b1276a71d94928
Panos Michail <michailpanagiotis@gmail.com>
2025-09-26 09:16:42 +0200

    Backups

--- scripts/sync_backup.sh
+++ scripts/sync_backup.sh
@@ -4,7 +4,7 @@ set -e
 
 for dir in $(find ~/Backups/ -depth -maxdepth 1 -mindepth 1 -type d); do
   echo Syncing "$dir"
-  rsync -avzt --inplace --no-perms --no-owner --no-group --progress --delete "$dir" pi:/mnt/external/
+  rsync -avzt --inplace --no-perms --no-owner --no-group --progress --delete "$dir" personal:Backups/
 done
 
 curl https://kuma.duvve.gr/api/push/YmoXCIb6Y7\?status\=up\&msg\=OK\&ping\=
